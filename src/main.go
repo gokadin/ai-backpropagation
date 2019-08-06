91496bbf4eb08ffd7631357ab08a8b1666861133
Givi Odikadze <givi.odikadze@gmail.com>
2019-08-05 21:58:27 -0400

    small correction

--- src/main.go
+++ src/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"github.com/gokadin/ann-core/core"
 	"math/rand"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
+
+	n := core.NewNetwork()
+	n.AddInputLayer(2).
+		AddHiddenLayer(2).
+		AddOutputLayer(2)
 }
